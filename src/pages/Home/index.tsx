8665f047f7e2a0058aaa1139f4574f0ddb260fe7
Yashi Srivastava <yashi.srivastava131999@gmail.com>
2021-10-30 23:41:24 +0530

    added projet to repo

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,58 @@
+import { lazy } from "react";
+import IntroContent from "../../content/IntroContent.json";
+import MiddleBlockContent from "../../content/MiddleBlockContent.json";
+import AboutContent from "../../content/AboutContent.json";
+import MissionContent from "../../content/MissionContent.json";
+import ConnectContent from "../../content/ConnectContent.json";
+import ContactContent from "../../content/ContactContent.json";
+
+const Contact = lazy(() => import("../../components/ContactForm"));
+const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
+const Container = lazy(() => import("../../common/Container"));
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
+const ContentBlock = lazy(() => import("../../components/ContentBlock"));
+
+const Home = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        type="right"
+        title={IntroContent.title}
+        content={IntroContent.text}
+        button={IntroContent.button}
+        icon="girl_with_flag.svg"
+        id="intro"
+      />
+      <ContentBlock
+        type="left"
+        title={AboutContent.title}
+        content={AboutContent.text}
+        section={AboutContent.section}
+        icon="family.svg"
+        id="about"
+      />
+      <ContentBlock
+        type="right"
+        title={MissionContent.title}
+        content={MissionContent.text}
+        icon="make_change.svg"
+        id="mission"
+      />
+      <ContentBlock
+        type="left"
+        title={ConnectContent.title}
+        content={ConnectContent.text}
+        icon="deversity.svg"
+        id="connect"
+      />
+      <Contact
+        title={ContactContent.title}
+        content={ContactContent.text}
+        id="contact"
+      />
+    </Container>
+  );
+};
+
+export default Home;
